b85f18c2c5da260f8fa31303b667b96328d6ae2c
Rejoice-me <140878053+Rejoice-me@users.noreply.github.com>
2023-07-29 07:53:37 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Rejoice-me
+- 👀 I’m interested in learning more
+- 🌱 I’m currently learning how to design
+- 💞️ I’m looking to collaborate on designing works
+- 📫 How to reach me call me +2348037938713
+
+<!---
+Rejoice-me/Rejoice-me is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
